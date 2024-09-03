59699c3d133e699d5abfb1400750386cb8188c2e
Nick Black <nickblack@VELO-V201849.local>
2024-09-03 16:11:13 -0500

    Initial Commit

---
+++ Athena/AthenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaApp.swift
+//  Athena
+//
+//  Created by Nick Black on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AthenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
